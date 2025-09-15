6a89888fe1c5f3a2d67b4438e3c36adee39f1be6
ifaakash <aakashc.appregis@gmail.com>
2025-09-15 18:22:01 +0530

    chore(networking): Remove default values from variables.tf and pass from QuickSpin repo

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -1,31 +1,26 @@
 variable "prefix" {
   description = "Prefix for resource names"
   type        = string
-  default     = "quickspin"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
   description = "CIDR block for Public subnet"
   type        = string
-  default     = "10.0.0.0/22"
 }
 
 variable "enable_dns_hostnames" {
   description = "Enable DNS hostnames for the VPC"
   type        = bool
-  default     = true
 }
 
 variable "enable_dns_support" {
   description = "Enable DNS Support for the VPC"
   type        = bool
-  default     = true
 }
 
 variable "user_ip" {
@@ -36,8 +31,4 @@ variable "user_ip" {
 variable "default_tags" {
   description = "Default tags for resources deployment"
   type        = map(string)
-  default = {
-    "Project"    = "QuickSpin"
-    "Created_by" = "ifaakash"
-  }
 }
