f09fc8b8e4adb7f93a17b485aed5b77f3db965b0
ifaakash <aakashc.appregis@gmail.com>
2025-10-22 12:27:25 +0530

    fix(nic): Remove separate `sg` information from the configuration
    - Remove the security group information for the EC2 configuration

--- EC2/instance/main.tf
+++ EC2/instance/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "example" {
   primary_network_interface {
     network_interface_id = var.network_interface_id
   }
-  vpc_security_group_ids = var.security_group_ids
-  force_destroy          = true
-  tags                   = merge({ "Name" : "${var.prefix}-instance" }, var.default_tags)
+  # vpc_security_group_ids = var.security_group_ids
+  force_destroy = true
+  tags          = merge({ "Name" : "${var.prefix}-instance" }, var.default_tags)
 }
