2c3af718c00c802f8da5c26bdf0add6a599abb09
ifaakash <aakashc.appregis@gmail.com>
2025-10-26 16:41:48 +0530

    chore(networking): Update the security group rule name
    - The VPC endpoints allow inbound from the Instance security group, as the request will be raised by the SSM agent, running in the EC2
    - When the SSM raise a request, it goes as inbound to the endpoints, and thus they handle the connectivity

--- Networking/networking/vpc-endpoints.tf
+++ Networking/networking/vpc-endpoints.tf
@@ -5,14 +5,14 @@ resource "aws_security_group" "endpoint_sg" {
   tags        = merge({ "Name" : "${var.prefix}-sg" }, var.default_tags)
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress_main" {
+resource "aws_vpc_security_group_ingress_rule" "endpoints_ingress" {
   from_port         = 443
   to_port           = 443
   ip_protocol       = "tcp"
   security_group_id = aws_security_group.main.id # Allow traffic from the Instance security group
 }
 
-resource "aws_vpc_security_group_egress_rule" "egress_main" {
+resource "aws_vpc_security_group_egress_rule" "endpoints_egress" {
   security_group_id = aws_security_group.main.id
   ip_protocol       = "-1" # semantically equivalent to all ports
   cidr_ipv4         = "0.0.0.0/0"
