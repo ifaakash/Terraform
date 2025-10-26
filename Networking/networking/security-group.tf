d4944f121d49ca766ed2f9d56048227f6e66e753
ifaakash <aakashc.appregis@gmail.com>
2025-10-26 16:26:50 +0530

    chore(endpoints): Allow inbound traffic to VPC endpoints
    - The vpc endpoints sends inbound traffic to the instance SSM agent, to initiate the connection
    - The VPC endpoints alone have a security group, ENI attached to them. We need to allow outbound traffic ( 443 ) to the internal aws network ( so that endpoints can talk to SSM agent )

--- Networking/networking/security-group.tf
+++ Networking/networking/security-group.tf
@@ -6,16 +6,17 @@ resource "aws_security_group" "main" {
   tags = merge({ "Name" : "${var.prefix}-sg" }, var.default_tags)
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress_main" {
-  security_group_id = aws_security_group.main.id
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "tcp"
-  cidr_ipv4         = var.user_ip
-}
-
 resource "aws_vpc_security_group_egress_rule" "egress_main" {
   security_group_id = aws_security_group.main.id
   ip_protocol       = "-1" # semantically equivalent to all ports
   cidr_ipv4         = "0.0.0.0/0"
 }
+
+
+# resource "aws_vpc_security_group_ingress_rule" "ingress_main" {
+#   security_group_id = aws_security_group.main.id
+#   from_port         = 22
+#   to_port           = 22
+#   ip_protocol       = "tcp"
+#   cidr_ipv4         = var.user_ip
+# }
--- Networking/networking/vpc-endpoints.tf
+++ Networking/networking/vpc-endpoints.tf
@@ -1,8 +1,28 @@
+resource "aws_security_group" "endpoint_sg" {
+  name        = "${var.prefix}-vpc-endpoints-sg"
+  description = "Security group for VPC endpoints"
+  vpc_id      = aws_vpc.main.id
+  tags        = merge({ "Name" : "${var.prefix}-sg" }, var.default_tags)
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress_main" {
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+  security_group_id = aws_security_group.main.id # Allow traffic from the Instance security group
+}
+
+resource "aws_vpc_security_group_egress_rule" "egress_main" {
+  security_group_id = aws_security_group.main.id
+  ip_protocol       = "-1" # semantically equivalent to all ports
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
 resource "aws_vpc_endpoint" "ssm" {
   vpc_id              = aws_vpc.main.id
   service_name        = "com.amazonaws.us-east-1.ssm"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.main.id]
+  security_group_ids  = [aws_security_group.endpoint_sg.id]
   subnet_ids          = [aws_subnet.private.id]
   private_dns_enabled = true
 }
@@ -11,7 +31,7 @@ resource "aws_vpc_endpoint" "ssm_messages" {
   vpc_id              = aws_vpc.main.id
   service_name        = "com.amazonaws.us-east-1.ssmmessages"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.main.id]
+  security_group_ids  = [aws_security_group.endpoint_sg.id]
   subnet_ids          = [aws_subnet.private.id]
   private_dns_enabled = true
 }
@@ -20,7 +40,7 @@ resource "aws_vpc_endpoint" "ec2_messages" {
   vpc_id              = aws_vpc.main.id
   service_name        = "com.amazonaws.us-east-1.ec2messages"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.main.id]
+  security_group_ids  = [aws_security_group.endpoint_sg.id]
   subnet_ids          = [aws_subnet.private.id]
   private_dns_enabled = true
 }
