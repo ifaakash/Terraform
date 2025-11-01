7eaf4c0654201c9a805183e3e3849a4ba3464045
ifaakash <aakashc.appregis@gmail.com>
2025-11-01 22:50:08 +0530

    fix(output): Add the required output variables
    - These will be consumed by `NIC` module, to deploy multiple ENI's

--- Networking/output.tf
+++ Networking/output.tf
@@ -6,6 +6,16 @@ output "security_group_id" {
   value = module.networking.security_group_id
 }
 
+output "public_subnet_id" {
+  value       = module.networking.public_subnet_id
+  description = "ID of the public subnet"
+}
+
+output "private_subnet_id" {
+  value       = module.networking.private_subnet_id
+  description = "ID of the private subnet"
+}
+
 # output "network_interface_id" {
 #   value       = aws_network_interface.nic.id
 #   description = "Network Interface for networking"
